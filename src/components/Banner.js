c5f535172f12212d403e1af6236c8fec00f87799
Kyoo130 <kml9003@kakao.com>
2022-04-27 21:56:44 +0900

    :lipstick: 배너 스타일 수정

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 
 const Banner = ({ movie }) => {
   return (
-    <div
-      className="banner"
+    <BannerCont
       style={{
         backgroundImage: `url(https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${movie.poster_path})`,
       }}
     >
-      <div className="banner-info">
+      <BannerInfo>
         <h1>{movie.title}</h1>
         <p>{movie.overview}</p>
-      </div>
-    </div>
+      </BannerInfo>
+    </BannerCont>
   );
 };
+
+const BannerCont = styled.div`
+  display: flex;
+  align-items: center;
+  background-position: center;
+  background-size: cover;
+  height: 600px;
+  
+  :before {
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 600px;
+    content: "";
+    background: linear-gradient(to right, black, transparent);
+  }
+`;
+
+const BannerInfo = styled.div`
+  width: 45%;
+  margin-left: 3rem;
+  z-index: 1;
+`;
 
 export default Banner;
